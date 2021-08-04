6815c4db8ad3418d2b9ea30709ad45ca7b64da53
JackGre <gl.meh.bmes.od@gmail.com>
2021-08-04 19:00:19 +0300

    Good

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import styles from "./App.module.css";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -47,7 +42,20 @@ class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
-  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
 
   render() {
